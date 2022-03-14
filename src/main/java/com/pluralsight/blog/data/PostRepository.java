890d8bbba1840a8293d72c66ebbe0e16a747857e
Adrian Filpescu <adrian.filipescu@gmail.com>
2022-03-14 15:53:21 +0200

    interfaces

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,6 +3,7 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -25,4 +26,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 //    public Optional<Post> findById(Long id) {
 //        return null;
 //    }
-}
+@RestResource(rel="contains-title", path="containsTitle")
+//http://localhost:8080/posts/search/containsTitle?title=Smart
+    List<Post> findByTitleContaining(String title);
